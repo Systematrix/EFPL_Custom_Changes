a1381761b08da7dc5f4b199a2f045687f6443ece
EXERP <admin@exotic-fruit.com>
2016-11-29 16:07:57 +0530

    CHanges on 29.11.16

---
+++ exotic_fruits/exotic_fruits/doctype/monthly_shipment_schedule/monthly_shipment_schedule.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Systematrix and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Monthly Shipment Schedule', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ exotic_fruits/exotic_fruits/doctype/monthly_shipment_schedule/test_monthly_shipment_schedule.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, Systematrix and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+# test_records = frappe.get_test_records('Monthly Shipment Schedule')
+
+class TestMonthlyShipmentSchedule(unittest.TestCase):
+	pass
